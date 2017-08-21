a16a524ddc9b47d48488fe28569164a82ff5f3de
Oskar Polak <oskarbank@gmail.com>
2017-08-21 17:03:40 +0200

    asd

--- src/pl/oskarpolak/weatherapp/AppStarter.java
+++ src/pl/oskarpolak/weatherapp/AppStarter.java
@@ -3,17 +3,35 @@ package pl.oskarpolak.weatherapp;
 
 import pl.oskarpolak.weatherapp.service.WeatherService;
 
+import java.util.Scanner;
+
 /**
  * Created by Lenovo on 21.08.2017.
  */
 public class AppStarter {
     public static void main(String[] args) {
-        new AppStarter();
+        new AppStarter().start();
     }
 
-    WeatherService weatherService = WeatherService.getService();
-
+    private WeatherService weatherService = WeatherService.getService();
+    private Scanner scanner;
     public AppStarter() {
-        weatherService.init("Cracow");
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        String response;
+        do{
+            System.out.print("Wpisz nazwę miasta: ");
+            response = scanner.nextLine();
+
+            if(response.equalsIgnoreCase("exit")){
+                break;
+            }
+
+            weatherService.init(response);
+
+        }while (true);
+        System.out.println("Papa!");
     }
 }
